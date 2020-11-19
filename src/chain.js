51845276a754ae920420d1a681605106409c006b
gpoliko <gpoliko@gmail.com>
2020-11-19 19:07:22 +1300

    Implement genesis block

--- src/chain.js
+++ src/chain.js
@@ -1,8 +1,20 @@
+const { generateKeyPair } = require("crypto");
 const { realpathSync, readSync } = require("fs");
 
 class Chain {
-    
+
     constructor () {
-        this.Chain
+        this.blockchain = [this.generateGenesis()]
+    }
+
+    generateTimeStamp() {
+        let date = new Date()
+        const { getFullYear, getMonth, getDate, getHours, getMinutes, getSeconds } = date
+        const timeStamp = `${getDate}-${getMonth}-${getFullYear}--${getHours}:${getMinutes}:${getSeconds}`
+        return timeStamp
+    }
+
+    generateGenesis () {
+        return new Block(0, this.generateTimeStamp, 'Genesis Block - First Block in the sick JS-CHAIN', '0')
     }
 }
