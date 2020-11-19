f6d23b59df23889b3c4e1353ac74f8b4a791cf52
gpoliko <gpoliko@gmail.com>
2020-11-19 19:15:56 +1300

    Add function to create new blocks

--- src/chain.js
+++ src/chain.js
@@ -1,5 +1,3 @@
-const { generateKeyPair } = require("crypto");
-const { realpathSync, readSync } = require("fs");
 
 class Chain {
 
@@ -7,6 +5,7 @@ class Chain {
         this.blockchain = [this.generateGenesis()]
     }
 
+    // Generates a time to record the creation time of a new block
     generateTimeStamp() {
         let date = new Date()
         const { getFullYear, getMonth, getDate, getHours, getMinutes, getSeconds } = date
@@ -14,7 +13,21 @@ class Chain {
         return timeStamp
     }
 
+    // Generates the first block of the blockchain
     generateGenesis () {
         return new Block(0, this.generateTimeStamp, 'Genesis Block - First Block in the sick JS-CHAIN', '0')
+    }
+
+    // Returns the newest block in the blockchain.
+    // Helper method for validation when adding a new block
+    getCurrentBlock () {
+        return this.blockchain[this.blockchain - 1]
+    }
+
+    // Create
+    createNewBlock (newBlock) {
+        newBlock.previousHash = this.getCurrentBlock().hash
+        newBlock.has = newBlocklock.generateHash()
+        this.blockchain.push(newBlock)
     }
 }
