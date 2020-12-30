c837e0e45f6ee5aedc8dafb7d9b57dd1b8db66c4
Giovanni Poliko <60165179+gpoliko@users.noreply.github.com>
2020-12-31 12:38:05 +1300

    gut contents to fill with correct actions

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,19 +1,1 @@
-import { fetchAllBeers } from '../api/api'
 
-export const NAVIGATE = 'NAVIGATE'
-export const ADD_TO_CART = 'ADD_TO_CART'
-
-export const navigate = target => {
-  return {
-    type: NAVIGATE,
-    target: target
-  }
-}
-
-export const addToCart = (id, name) => {
-  return {
-    type: ADD_TO_CART,
-    id: id,
-    name: name
-  }
-}
